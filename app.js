6717559ca9805bc162f36039d6bba61777d79411
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2022-11-16 13:28:51 -0800

    added app.js file and linked to index

---
+++ app.js
@@ -0,0 +1,8 @@
+'use strict'
+
+console.log('hello world')
+
+  //https://tinyurl.com/dynamic-html-checker
+  document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
+      
+  document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href); 
