4518ecb23fa552ef281ae45f3c6f2de137f86ef5
Matt <josematthewmiguel@gmail.com>
2022-11-21 13:06:52 -0800

    changed to the reading

--- app.js
+++ app.js
@@ -2,27 +2,20 @@
 
 // console.log('hello world')
 
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
+let myDate = new Date();
+let myHour = myDate.getHours();
 
-console.log(hourNow)
 
-if (hourNow > 18) { 
-    greeting = 'Good evening!';
-
-} else if (hourNow > 12) {
-    greeting = 'Good afternoon!';
-
-} else if (hourNow > 0) {
-    greeting = 'Good morning';
+if (myHour > 12) {
+    // alert('Good Afternoon!')
 } else {
-    greeting = 'Welcome';
+    // alert('NOT afternoon')
 }
+
 
 document.write('<h3>' + greeting + '</h3>');
 
-  //https://tinyurl.com/dynamic-html-checker
-  document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
-      
-  document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href); 
+//https://tinyurl.com/dynamic-html-checker
+document.getElementById("html-checker").setAttribute("href", "https://validator.w3.org/nu/?doc=" + location.href);
+
+document.getElementById("css-checker").setAttribute("href", "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href); 
